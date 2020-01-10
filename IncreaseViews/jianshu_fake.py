f912b5b47bc4e84c3f8c30fa92b52bff67609b5a
levinyi <dushiyi319@163.com>
2020-01-10 09:20:45 +0800

    Create jianshu_fake.py

---
+++ IncreaseViews/jianshu_fake.py
@@ -0,0 +1,52 @@
+import time
+import socket
+import random
+import urllib
+
+socket.setdefaulttimeout(3)
+user_agent_list = [
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/45.0.2454.85 Safari/537.36 115Browser/6.0.3',
+    'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
+    'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
+    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)',
+    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)',
+    'Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1',
+    'Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11',
+    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SE 2.X MetaSr 1.0; SE 2.X MetaSr 1.0; .NET CLR 2.0.50727; SE 2.X MetaSr 1.0)',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0',
+    'Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1',
+]
+f = open("proxy","r")
+lines = f.readlines()
+proxys = []
+
+for i in range(0, len(lines)):
+    ip = lines[i].strip("\n").split(",")
+    proxy_host = 'http://' + ip[0] + ':' + ip[1]
+    print(proxy_host)
+    proxy_temp = {"http": proxy_host}
+    proxys.append(proxy_temp)
+urls = [
+    "https://www.jianshu.com/p/4131fbf8cd3e",
+]
+
+j = 1
+for i in range(100):
+    for proxy in proxys:
+        for url in urls:
+            try:
+                user_agent = random.choice(user_agent_list)
+                proxy_support = urllib.request.ProxyHandler(proxy)
+                opener = urllib.request.build_opener(proxy_support, urllib.request.HTTPHandler)
+                urllib.request.install_opener(opener)
+                req = urllib.request.Request(url)
+                c = urllib.request.urlopen(req)
+                print("successfully", j)
+                j += 1
+                time.sleep(5)
+            except Exception as e:
+                print(proxy)
+                print(e)
+                continue
